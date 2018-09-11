d6912afacb0b67a2adaeb5e1612c5cec55a6112f
Andrews, Joshua M <joshua.m.andrews4.ctr@mail.mil>
2018-09-11 13:47:50 -0500

    Tests: adding tests init

---
+++ KredoKodo.Library.Excel.Tests/ReportGeneratorTestBase.cs
@@ -0,0 +1,164 @@
+﻿using ClosedXML.Excel;
+using KredoKodo.Library.Excel.ReportGenerator;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+
+namespace KredoKodo.Library.Excel.Tests
+{
+    public abstract class ReportGeneratorTestBase : IDisposable
+    {
+        protected ReportGeneratorTestBase()
+        {
+            SampleDataTable = GenerateSampleDataTable();
+            SampleReportDataTable = GenerateReportDataTable();
+        }
+
+        protected DataTable SampleDataTable { get; set; }
+        protected DataTable SampleReportDataTable { get; set; }
+
+        public void Dispose()
+        {
+            SampleDataTable.Dispose();
+            SampleReportDataTable.Dispose();
+        }
+
+        private DataTable GenerateSampleDataTable()
+        {
+            var dataTable = new DataTable();
+
+            dataTable.Columns.Add("Quantity", typeof(int));
+            dataTable.Columns.Add("Item", typeof(string));
+            dataTable.Columns.Add("Person", typeof(string));
+            dataTable.Columns.Add("Date", typeof(DateTime));
+
+            dataTable.Rows.Add(25, "Cookie", "David", default(DateTime));
+            dataTable.Rows.Add(50, "Rice", "Stephen", default(DateTime));
+            dataTable.Rows.Add(10, "Brat", "Joshua", default(DateTime));
+            dataTable.Rows.Add(21, "Chip", "Rev", default(DateTime));
+            dataTable.Rows.Add(100000, "$100,000 bar", "Ken", DateTime.Parse("03/03/03 12:59 PM"));
+
+            return dataTable;
+        }
+
+        private DataTable GenerateReportDataTable()
+        {
+            var dataTable = new DataTable();
+
+            dataTable.Columns.Add("Count", typeof(int));
+            dataTable.Columns.Add("Dev", typeof(string));
+            dataTable.Columns.Add("Food", typeof(string));
+            dataTable.Columns.Add("Purchase Date", typeof(DateTime));
+
+            dataTable.Rows.Add(25, "David", "Cookie", default(DateTime));
+            dataTable.Rows.Add(50, "Stephen", "Rice", default(DateTime));
+            dataTable.Rows.Add(10, "Joshua", "Brat", default(DateTime));
+            dataTable.Rows.Add(21, "Rev", "Chip", default(DateTime));
+            dataTable.Rows.Add(100000, "Ken", "$100,000 bar", DateTime.Parse("03/03/03 12:59 PM"));
+
+            return dataTable;
+        }
+
+        protected XLWorkbook GenerateExpectedWorkbook(
+            DataTable dataTable,
+            List<ColumnModel> formatList)
+        {
+            var workbook = new XLWorkbook();
+            var configuredWorksheet = workbook
+                .AddWorksheet(dataTable, "test");
+
+            configuredWorksheet.Style.Font.FontSize = 10;
+            configuredWorksheet
+                .Style
+                .Font
+                .FontFamilyNumbering = XLFontFamilyNumberingValues.Modern;
+
+            foreach (var customFormat in formatList)
+            {
+                var columnOrdinal = -1;
+                foreach (DataColumn column in dataTable.Columns)
+                {
+                    if (column.ColumnName == customFormat.ColumnName)
+                    {
+                        columnOrdinal = column.Ordinal;
+                    }
+                }
+
+                if (columnOrdinal > -1)
+                {
+                    if (!string.IsNullOrWhiteSpace(customFormat.ColumnFormat))
+                    {
+                        configuredWorksheet
+                            .Column(columnOrdinal + 1)
+                            .Style
+                            .NumberFormat
+                            .Format = customFormat.ColumnFormat;
+                    }
+
+                    if (customFormat.IsColumnCentered)
+                    {
+                        configuredWorksheet
+                            .Column(columnOrdinal + 1)
+                            .Style
+                            .Alignment
+                            .Horizontal = XLAlignmentHorizontalValues.Center;
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(customFormat.ColumnBackgroundColor))
+                    {
+                        configuredWorksheet
+                            .Column(columnOrdinal + 1)
+                            .Style
+                            .Fill
+                            .BackgroundColor = XLColor.FromHtml(customFormat.ColumnBackgroundColor);
+
+                        // Remove any column formatting on the header row, 
+                        // so the header row is the same for all columns
+                        configuredWorksheet.Cell(1, columnOrdinal + 1)
+                            .Clear(XLClearOptions.AllFormats);
+                    }
+                }
+            }
+
+            return workbook;
+        }
+
+        protected DataTable SampleQuantityDataTable()
+        {
+            var dataTable = new DataTable();
+
+            dataTable.Columns.Add("Count", typeof(decimal));
+
+            dataTable.Rows.Add(25);
+            dataTable.Rows.Add(50);
+            dataTable.Rows.Add(10);
+            dataTable.Rows.Add(21);
+            dataTable.Rows.Add(100000);
+            dataTable.Rows.Add(35.2);
+
+            return dataTable;
+        }
+
+        protected DataTable SampleDatesDataTable()
+        {
+            var dataTable = new DataTable();
+
+            dataTable.Columns.Add("Service Date", typeof(DateTime));
+
+            dataTable.Rows.Add(default(DateTime));
+            dataTable.Rows.Add(DateTime.Parse("03/03/2003"));
+            dataTable.Rows.Add(DateTime.Parse("09/05/2010 09:30 PM"));
+
+            return dataTable;
+        }
+
+        protected MemoryStream CreateMemoryStream(XLWorkbook workbook)
+        {
+            var memStream = new MemoryStream();
+            workbook.SaveAs(memStream);
+
+            return memStream;
+        }
+    }
+}
---
+++ KredoKodo.Library.Excel.Tests/Test_ReportGenerator.cs
@@ -0,0 +1,383 @@
+﻿using ClosedXML.Excel;
+using KredoKodo.Library.Excel.ReportGenerator;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using Xunit;
+
+namespace KredoKodo.Library.Excel.Tests
+{
+    [Trait("Type", "ReportGenerator Excel Tests")]
+    public class Test_ReportGenerator : ReportGeneratorTestBase
+    {
+        private readonly ReportGenerator.ReportGenerator subject;
+
+        // TODO: Build and add equality library.
+        //private readonly ExcelEquality excelEquality;
+
+        public Test_ReportGenerator() : base()
+        {
+            subject = new ReportGenerator.ReportGenerator();
+            //excelEquality = new ExcelEquality();
+        }
+
+        [Fact(DisplayName = "ReportGenerator: a datatable with no columns throws")]
+        public void RequiresAtLeastOneColumn()
+        {
+            var emptyDatatable = new DataTable();
+
+            Should.Throw<InvalidOperationException>(() =>
+                subject
+                    .AddWorksheet(emptyDatatable, "test")
+                        .FinalizeWorksheet()
+                    .GenerateWorkbook())
+                .Message
+                .ShouldBe(
+                    "You must provide a DataTable with at least one column.");
+        }
+
+        [Fact(DisplayName = "ReportGenerator: ConfigureColumn requires at least one column")]
+        public void ConfigureColumnRequiresAtLeastOneColumn()
+        {
+            Should.Throw<InvalidOperationException>(() =>
+                subject
+                    .AddWorksheet(SampleDataTable, "test")
+                        .FinalizeWorksheet())
+            .Message
+            .ShouldBe("You must configure at least one column.");
+        }
+
+        [Fact(DisplayName = "ReportGenerator: requires a worksheet name")]
+        public void RequireWorksheetName()
+        {
+            Should.Throw<ArgumentException>(() =>
+               subject.AddWorksheet(SampleDataTable, ""))
+            .Message
+            .ShouldBe("You must provide a worksheet name.");
+        }
+
+        [Fact(DisplayName = "ReportGenerator: GenerateWorkbook returns XLWorkbook")]
+        public void GenerateWorkbookReturnsXLWorkbook()
+        {
+            var testWorkbook = subject
+                .AddWorksheet(SampleQuantityDataTable(), "test")
+                    .ConfigureColumn("Count", "Count")
+                    .FinalizeWorksheet()
+                .GenerateWorkbook();
+
+            testWorkbook.ShouldBeOfType<XLWorkbook>();
+        }
+
+        [Fact(DisplayName = "ReportGenerator: GenerateWorkbook returns XLWorkbook bytes")]
+        public void GenerateWorkbookReturnsXLWorkbookBytes()
+        {
+            var testWorkbook = subject
+                .AddWorksheet(SampleQuantityDataTable(), "test")
+                    .ConfigureColumn("Count", "Count")
+                    .FinalizeWorksheet()
+                .GenerateWorkbookBytes();
+
+            testWorkbook.ShouldBeOfType<byte[]>();
+        }
+
+        //[Fact(DisplayName = "ReportGenerator: GenerateWorkbook returns quantity formatted XLWorkbook")]
+        //public void GenerateWorkbookReturnsQuantityFormattedXLWorkbook()
+        //{
+        //    var formatList = new List<ColumnModel>()
+        //    {
+        //        new ColumnModel
+        //        {
+        //            ColumnFormat = CustomFormat.Quantity,
+        //            ColumnName = "Count"
+        //        }
+        //    };
+
+        //    var testWorkbook = subject
+        //        .AddWorksheet(SampleQuantityDataTable(), "test")
+        //            .ConfigureColumn("Count", "Count", CustomFormat.Quantity)
+        //            .FinalizeWorksheet()
+        //        .GenerateWorkbook();
+
+        //    var streams = new Stream[]
+        //    {
+        //        CreateMemoryStream(testWorkbook),
+        //        CreateMemoryStream(GenerateExpectedWorkbook(
+        //            SampleQuantityDataTable(),
+        //            formatList))
+        //    };
+
+        //    excelEquality
+        //        .StreamsAreEqual(streams)
+        //        .AreEqual
+        //        .ShouldBeTrue();
+        //}
+
+        //[Fact(DisplayName = "ReportGenerator: GenerateWorkbook returns currency 0 decimal formatted XLWorkbook")]
+        //public void GenerateWorkbookReturns0DecimalFormattedXLWorkbook()
+        //{
+        //    var formatList = new List<ColumnModel>()
+        //    {
+        //        new ColumnModel
+        //        {
+        //            ColumnFormat = CustomFormat.Currency_0_Decimals,
+        //            ColumnName = "Count"
+        //        }
+        //    };
+
+        //    var testWorkbook = subject
+        //        .AddWorksheet(SampleQuantityDataTable(), "test")
+        //            .ConfigureColumn("Count", "Count", CustomFormat.Currency_0_Decimals)
+        //            .FinalizeWorksheet()
+        //        .GenerateWorkbook();
+
+        //    var streams = new Stream[]
+        //    {
+        //        CreateMemoryStream(testWorkbook),
+        //        CreateMemoryStream(GenerateExpectedWorkbook(
+        //            SampleQuantityDataTable(),
+        //            formatList))
+        //    };
+
+        //    excelEquality
+        //        .StreamsAreEqual(streams)
+        //        .AreEqual
+        //        .ShouldBeTrue();
+        //}
+
+        //[Fact(DisplayName = "ReportGenerator: GenerateWorkbook returns currency 2 decimal formatted XLWorkbook")]
+        //public void GenerateWorkbookReturns2DecimalFormattedXLWorkbook()
+        //{
+        //    var formatList = new List<ColumnModel>()
+        //    {
+        //        new ColumnModel
+        //        {
+        //            ColumnFormat = CustomFormat.Currency_2_Decimals,
+        //            ColumnName = "Count"
+        //        }
+        //    };
+
+        //    var testWorkbook = subject
+        //        .AddWorksheet(SampleQuantityDataTable(), "test")
+        //            .ConfigureColumn("Count", "Count", CustomFormat.Currency_2_Decimals)
+        //            .FinalizeWorksheet()
+        //        .GenerateWorkbook();
+
+        //    var streams = new Stream[]
+        //    {
+        //        CreateMemoryStream(testWorkbook),
+        //        CreateMemoryStream(GenerateExpectedWorkbook(
+        //            SampleQuantityDataTable(),
+        //            formatList))
+        //    };
+
+        //    excelEquality
+        //        .StreamsAreEqual(streams)
+        //        .AreEqual
+        //        .ShouldBeTrue();
+        //}
+
+        //[Fact(DisplayName = "ReportGenerator: GenerateWorkbook returns currency 4 decimal formatted XLWorkbook")]
+        //public void GenerateWorkbookReturns4DecimalFormattedXLWorkbook()
+        //{
+        //    var formatList = new List<ColumnModel>()
+        //    {
+        //        new ColumnModel
+        //        {
+        //            ColumnFormat = CustomFormat.Currency_4_Decimals,
+        //            ColumnName = "Count"
+        //        }
+        //    };
+
+        //    var testWorkbook = subject
+        //        .AddWorksheet(SampleQuantityDataTable(), "test")
+        //            .ConfigureColumn("Count", "Count", CustomFormat.Currency_4_Decimals)
+        //            .FinalizeWorksheet()
+        //        .GenerateWorkbook();
+
+        //    var streams = new Stream[]
+        //    {
+        //        CreateMemoryStream(testWorkbook),
+        //        CreateMemoryStream(GenerateExpectedWorkbook(
+        //            SampleQuantityDataTable(),
+        //            formatList))
+        //    };
+
+        //    excelEquality
+        //        .StreamsAreEqual(streams)
+        //        .AreEqual
+        //        .ShouldBeTrue();
+        //}
+
+        //[Fact(DisplayName = "ReportGenerator: GenerateWorkbook returns shortdate formatted XLWorkbook")]
+        //public void GenerateWorkbookReturnsShortDateFormattedXLWorkbook()
+        //{
+        //    var formatList = new List<ColumnModel>()
+        //    {
+        //        new ColumnModel
+        //        {
+        //            ColumnFormat = CustomFormat.ShortDate,
+        //            ColumnName = "Service Date"
+        //        }
+        //    };
+
+        //    var testWorkbook = subject
+        //        .AddWorksheet(SampleDatesDataTable(), "test")
+        //            .ConfigureColumn("Service Date", "Service Date", CustomFormat.ShortDate)
+        //            .FinalizeWorksheet()
+        //        .GenerateWorkbook();
+
+        //    var streams = new Stream[]
+        //    {
+        //        CreateMemoryStream(testWorkbook),
+        //        CreateMemoryStream(GenerateExpectedWorkbook(
+        //            SampleDatesDataTable(),
+        //            formatList))
+        //    };
+
+        //    excelEquality
+        //        .StreamsAreEqual(streams)
+        //        .AreEqual
+        //        .ShouldBeTrue();
+        //}
+
+        //[Fact(DisplayName = "ReportGenerator: GenerateWorkbook returns center formatted XLWorkbook")]
+        //public void GenerateWorkbookReturnsCenterFormattedXLWorkbook()
+        //{
+        //    var formatList = new List<ColumnModel>()
+        //    {
+        //        new ColumnModel
+        //        {
+        //            IsColumnCentered = true,
+        //            ColumnName = "Service Date"
+        //        }
+        //    };
+
+        //    var testWorkbook = subject
+        //        .AddWorksheet(SampleDatesDataTable(), "test")
+        //            .ConfigureColumn("Service Date", "Service Date", isCentered: true)
+        //            .FinalizeWorksheet()
+        //        .GenerateWorkbook();
+
+        //    var streams = new Stream[]
+        //    {
+        //        CreateMemoryStream(testWorkbook),
+        //        CreateMemoryStream(GenerateExpectedWorkbook(
+        //            SampleDatesDataTable(),
+        //            formatList))
+        //    };
+
+        //    excelEquality
+        //        .StreamsAreEqual(streams)
+        //        .AreEqual
+        //        .ShouldBeTrue();
+        //}
+
+        //[Fact(DisplayName = "ReportGenerator: GenerateWorkbook returns black background formatted XLWorkbook")]
+        //public void GenerateWorkbookReturnsBlackBackgroundFormattedXLWorkbook()
+        //{
+        //    var formatList = new List<ColumnModel>()
+        //    {
+        //        new ColumnModel
+        //        {
+        //            ColumnBackgroundColor = "#000000",
+        //            ColumnName = "Service Date"
+        //        }
+        //    };
+
+        //    var testWorkbook = subject
+        //        .AddWorksheet(SampleDatesDataTable(), "test")
+        //            .ConfigureColumn("Service Date", "Service Date", backgroundColor: "#000000")
+        //            .FinalizeWorksheet()
+        //        .GenerateWorkbook();
+
+        //    var streams = new Stream[]
+        //    {
+        //        CreateMemoryStream(testWorkbook),
+        //        CreateMemoryStream(GenerateExpectedWorkbook(
+        //            SampleDatesDataTable(),
+        //            formatList))
+        //    };
+
+        //    excelEquality
+        //        .StreamsAreEqual(streams)
+        //        .AreEqual
+        //        .ShouldBeTrue();
+        //}
+
+        //[Fact(DisplayName = "ReportGenerator: GenerateWorkbook returns tan background formatted XLWorkbook")]
+        //public void GenerateWorkbookReturnsTanBackgroundFormattedXLWorkbook()
+        //{
+        //    var formatList = new List<ColumnModel>()
+        //    {
+        //        new ColumnModel
+        //        {
+        //            ColumnBackgroundColor = "#FFFFCC",
+        //            ColumnName = "Service Date"
+        //        }
+        //    };
+
+        //    var testWorkbook = subject
+        //        .AddWorksheet(SampleDatesDataTable(), "test")
+        //            .ConfigureColumn("Service Date", "Service Date", backgroundColor: "#FFFFCC")
+        //            .FinalizeWorksheet()
+        //        .GenerateWorkbook();
+
+        //    var streams = new Stream[]
+        //    {
+        //        CreateMemoryStream(testWorkbook),
+        //        CreateMemoryStream(GenerateExpectedWorkbook(
+        //            SampleDatesDataTable(),
+        //            formatList))
+        //    };
+
+        //    excelEquality
+        //        .StreamsAreEqual(streams)
+        //        .AreEqual
+        //        .ShouldBeTrue();
+        //}
+
+        [Fact(DisplayName = "ReportGenerator: ConfigureColumn throws human readable exception on mismatched name")]
+        public void ConfigureColumnThrowsHumanReadableOnColumnMismatch()
+        {
+            Should.Throw<ArgumentException>(() =>
+                subject
+                    .AddWorksheet(SampleDataTable, "test")
+                        .ConfigureColumn("totally wrong name", "new name")
+                        .FinalizeWorksheet()
+                    .GenerateWorkbook())
+            .Message
+            .ShouldBe("You must provide a matching DataColumn name as the first parameter.");
+        }
+
+        [Fact(DisplayName = "ReportGenerator: ConfigureColumn throws on invalid excel column name")]
+        public void ConfigureColumnThrowsOnInvalidExcelColumnName()
+        {
+            Should.Throw<ArgumentException>(() =>
+                subject
+                    .AddWorksheet(SampleDataTable, "test")
+                        .ConfigureColumn("Quantity", "")
+                        .FinalizeWorksheet()
+                    .GenerateWorkbook())
+            .Message
+            .ShouldBe("You must provide a valid Excel column name as the second parameter.");
+        }
+
+        [Fact(DisplayName = "ReportGenerator: Can add more than one worksheet")]
+        public void CanAddMoreThanOneWorksheet()
+        {
+            var workbook = subject
+                .AddWorksheet(SampleDataTable, "worksheet1")
+                    .ConfigureColumn("Quantity", "Count")
+                    .FinalizeWorksheet()
+                .AddWorksheet(SampleDataTable, "worksheet2")
+                    .ConfigureColumn("Item", "Food")
+                    .ConfigureColumn("Person", "Dev", backgroundColor: "#FFFFCC")
+                    .FinalizeWorksheet()
+                .GenerateWorkbook();
+
+            workbook.Worksheets.Count.ShouldBe(2);
+        }
+    }
+}
